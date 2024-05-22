8e23a68d251e67d3926c7be95071da5ad4a9e96c
Florence Martin <florence.martin@ingeli.fr>
2024-05-22 18:15:52 +0200

    Initial commit

--- Components/Home.tsx
+++ Components/Home.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, {useState} from "react";
+import { StyleSheet, View, Text} from "react-native";
+import { Picker } from '@react-native-picker/picker';
 import { StatusBar } from "expo-status-bar";
 import ImageViewer from "./ImageViewer";
 import { HomeProps } from "../Navigation/StackNavigation";
@@ -7,21 +8,41 @@ import { HomeProps } from "../Navigation/StackNavigation";
 const PlaceholderImage = require("../assets/images/accueil-image.jpeg");
 
 const Home: React.FC<HomeProps> = ({ navigation, route }) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.banner}>
-        <Text style={styles.bannerText}>Bienvenue &agrave; Simplonville</Text>
-        <Text style={styles.title}>
-          Alertez-nous !{"\n"} Accident, travaux, probl&egrave;me de voirie
-          (propret&eacute;, &eacute;clairage,...) !
-        </Text>
-      </View>
-      <View>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
-      </View>
-      <StatusBar style="auto" />
+const [selectedIncidentType, setSelectedIncidentType] = useState<string>("");
+const incidentTypes = [
+  { label: "Accident", value: "accident" },
+  { label: "Travaux", value: "travaux" },
+  { label: "Propreté", value: "proprete" },
+  { label: "Éclairage", value: "eclairage" },
+];
+
+return (
+  <View style={styles.container}>
+    <View style={styles.banner}>
+      <Text style={styles.bannerText}>Bienvenue à Lyon</Text>
+      <Text style={styles.title}>
+        Alertez-nous !{"\n"} Accident, travaux, problème de voirie
+        (propreté, éclairage,...) !
+      </Text>
     </View>
-  );
+    <View>
+      <ImageViewer placeholderImageSource={PlaceholderImage} />
+    </View>
+    <View style={styles.pickerContainer}>
+      <Text style={styles.pickerLabel}>Type d'incident</Text>
+      <Picker
+        selectedValue={selectedIncidentType}
+        style={styles.picker}
+        onValueChange={(itemValue) => setSelectedIncidentType(itemValue)}
+      >
+        {incidentTypes.map((type) => (
+          <Picker.Item key={type.value} label={type.label} value={type.value} />
+        ))}
+      </Picker>
+    </View>
+    <StatusBar style="auto" />
+  </View>
+);
 };
 
 export default Home;
@@ -29,31 +50,33 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 100,
   },
   banner: {
-    backgroundColor: "#383E42",
-    width: "100%",
-    height: 120,
-    alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 20,
   },
   bannerText: {
-    color: "white",
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 4,
-    marginTop: 4,
+    fontSize: 24,
+    fontWeight: 'bold',
   },
-
   title: {
-    color: "white",
-    width: "100%",
-    textAlign: "center",
+    fontSize: 18,
+    textAlign: 'center',
   },
-  image: {
-    width: 320,
-    height: 400,
+  pickerContainer: {
+    marginTop: 20,
+    width: '100%',
+    alignItems: 'center',
+  },
+  pickerLabel: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  picker: {
+    height: 50,
+    width: 200,
   },
 });
